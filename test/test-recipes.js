395c4a5c9b094ae44fbdf95febe6ea0c9b0a3920
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-08 23:30:37 -0400

    adds skeleton for tests

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -3,7 +3,27 @@ const chaiHttp = require('chai-http');
 
 describe('', function(){
 
-	it('should', function(){
+	it('should retrieve recipes', function(){
+
+		
+	});
+
+	it('should create new recipes', function(){
+
+		
+	});
+
+	it('should not create new recipe when the request is missing parameters', function(){
+
+		
+	});
+
+	it('should change an existing recipe', function(){
+
+		
+	});	
+
+	it('should delete an existing recipe', function(){
 
 		
 	});
