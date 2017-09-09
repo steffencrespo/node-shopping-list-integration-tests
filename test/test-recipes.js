7b58fdb90fb1536b0bc730239751b17c2e2ae8e5
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-09 14:08:16 -0400

    adds test for recipes get

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -1,15 +1,30 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const {app, runServer, closeServer} = require('../server');
+
 describe('', function(){
 
-	it('should retrieve recipes', function(){
+	before(function(){
+		return runServer();
+	});
 
+	after(function(){
+		return closeServer();
+	});
+
+	it('should retrieve recipes', function(){
+		return chai.request(app)
+				.get('/recipes')
+				.then(function(res) {
+					res.should.have.status(200);
+					res.should.be.json;
+					res.body.length.should.be.above(0);
+				});
 		
 	});
 
 	it('should create new recipes', function(){
-
 		
 	});
 
